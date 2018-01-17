ea972b41b707454fd5dc31febcd5af2a2d1d8177
AV <andrei.verdes@ffwagency.com>
2018-01-17 13:21:45 +0100

    all components ready for styling

--- test-website-app/src/app/contact-component/contact-component.component.ts
+++ test-website-app/src/app/contact-component/contact-component.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponentComponent implements OnInit {
 
+  sponsorTitle: string = 'Our happy Client';
+  sponsors: any[] = [{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'},{image:'image1'}];
+  formTitle: string = 'Give us a good news';
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+
   constructor() { }
 
   ngOnInit() {
