bc43ec5d8e39f393b137e1b4567c57e4f5509e89
AV <andrei.verdes@ffwagency.com>
2018-01-17 16:07:36 +0100

    slideshow progress

--- test-website-app/src/app/header-component/slideshow-component/slideshow-component.component.ts
+++ test-website-app/src/app/header-component/slideshow-component/slideshow-component.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import set = Reflect.set;
+
+
+declare var jquery: any;
+declare var $: any;
+
 
 @Component({
   selector: 'app-slideshow-component',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slideshow-component.component.scss']
 })
 export class SlideshowComponentComponent implements OnInit {
+  currentSlide = 0;
+  slides: { title: string, body: string, link: string }[] = [
+    {
+      title: "We Are Awesome Creative Agency",
+      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      link: "link"
+    },
+    {
+      title: "We love websites",
+      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      link: "link"
+    },
+    {
+      title: "We build the future",
+      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      link: "link"
+    },
+    {
+      title: "We need Coffee",
+      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      link: "link"
+    }
+  ];
+  constructor(){
+    console.log(this.currentSlide + ' current slide');
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+
+  ngOnInit() {
+    setTimeout(function () {
+      if (this.slides){
+        this.currentSlide +=1;
+        console.log(this.currentSlide + ' current slide');
+      }
+    },400);
+  }
 }
