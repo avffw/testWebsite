fa3eae06d60567ad0c9a9a09cd3619f23a256e91
AV <andrei.verdes@ffwagency.com>
2018-01-19 11:24:33 +0100

    almost done styling footer .. social media remaining

--- test-website-app/src/app/footer-component/footer-component.component.ts
+++ test-website-app/src/app/footer-component/footer-component.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-component.component.scss']
 })
 export class FooterComponentComponent implements OnInit {
+  logo: string = '../../assets/images/logos/footer_logo.png';
+  locationIcon: string = '../../assets/images/icons/location.png';
+  phoneIcon: string = '../../assets/images/icons/phone351.png';
+  socialIcons: any[] = [
+    {icon:'../../assets/images/social/dribbble4.png'},
+    {icon:'../../assets/images/social/facebook25.png'},
+    {icon:'../../assets/images/social/google26.png'},
+    {icon:'../../assets/images/social/instagram3.png'},
+    {icon:'../../assets/images/social/twitter16.png'},
+    {icon:'../../assets/images/social/youtube13.png'},
+    ];
+
+
+
+
+
+
+
+
+
 
   constructor() { }
 
