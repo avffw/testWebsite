dcf4b3ae0bd7d4089be97981a74bf686d81a5265
avffw <andrei.verdes@ffwagency.com>
2018-01-22 02:44:58 +0100

    slideshow basic working

--- test-website-app/src/app/header-component/slideshow-component/slideshow-component.component.ts
+++ test-website-app/src/app/header-component/slideshow-component/slideshow-component.component.ts
@@ -12,41 +12,69 @@ declare var $: any;
   styleUrls: ['./slideshow-component.component.scss']
 })
 export class SlideshowComponentComponent implements OnInit {
-  currentSlide = 0;
+  currentSlide: number;
+  defaultSlide: number = 0;
   slides: { title: string, body: string, link: string }[] = [
     {
       title: "We Are Awesome Creative Agency",
-      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      body: "1This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
       link: "link"
     },
     {
       title: "We love websites",
-      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      body: "2This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
       link: "link"
     },
     {
       title: "We build the future",
-      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      body: "3This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
       link: "link"
     },
     {
       title: "We need Coffee",
-      body: "This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
+      body: "4This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
       link: "link"
     }
   ];
-  constructor(){
-    console.log(this.currentSlide + ' current slide');
+
+  constructor() {
+    if (!this.currentSlide) {
+      this.currentSlide = this.defaultSlide;
+    }
+    setInterval(()=>{
+      this.changeSlide();
+      console.log(this.currentSlide);
+    },4000);
+    /*for (let i:any = 0; i < this.slides.length; i+=1) {
+      let limit:number = this.slides.length;
+      this.delaySlide(i,limit);
+      i = this.delaySlide(i,limit);
+    }*/
 
   }
 
 
   ngOnInit() {
-    setTimeout(function () {
-      if (this.slides){
-        this.currentSlide +=1;
-        console.log(this.currentSlide + ' current slide');
-      }
-    },400);
   }
+  delaySlide(i){
+
+  }
+  resetSlide(){
+    this.currentSlide = this.defaultSlide;
+  }
+  changeSlide() {
+    this.onChangeSlide();
+  }
+  onChangeSlide() {
+    console.log(this.currentSlide);
+    if(this.currentSlide !== this.slides.length-1){
+      this.currentSlide += 1;
+    }else {
+      this.currentSlide = this.defaultSlide;
+    }
+
+  }
+
+
+
 }
